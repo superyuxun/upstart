5e79f78e1b4ed0499e0304f0eefb38625eda2ef8
superyuxun <78048532+superyuxun@users.noreply.github.com>
2024-05-30 11:10:52 -0400

    Downloaded from Internet Archive

---
+++ circles_files/wp-colorbox.js
@@ -0,0 +1,44 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+jQuery(document).ready(function($) {
+    $(".wp-colorbox-image").colorbox({maxWidth:"99%", maxHeight: "99%"});
+    $(".wp-colorbox-youtube").colorbox({iframe:true, scrolling:false, width:"90%", height:"90%", maxWidth:640, maxHeight: 480});
+    $(".wp-colorbox-vimeo").colorbox({iframe:true, scrolling:false, width:"90%", height:"90%", maxWidth:640, maxHeight: 480});
+    $(".wp-colorbox-iframe").colorbox({iframe:true, width:"80%", height:"80%"});
+    $(".wp-colorbox-inline").colorbox({inline:true, width:"90%", maxWidth:640});
+});
+
+
+
+
+}
+/*
+     FILE ARCHIVED ON 08:21:13 Nov 16, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:56:05 May 30, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.635
+  exclusion.robots: 0.069
+  exclusion.robots.policy: 0.059
+  esindex: 0.01
+  cdx.remote: 7.072
+  LoadShardBlock: 342.772 (3)
+  PetaboxLoader3.datanode: 376.971 (4)
+  load_resource: 984.275
+  PetaboxLoader3.resolve: 426.943
+*/
